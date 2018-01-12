0ae2cfade6f90be327eb86f3a76b8b337cbf3e85
11223ren <32731986+11223ren@users.noreply.github.com>
2018-01-12 15:34:08 +0800

    完善了数据库备份和查询操作

--- app/admin/controller/SqlController.php
+++ app/admin/controller/SqlController.php
@@ -78,6 +78,8 @@ class SqlController extends AdminbaseController {
         $msqlback=new \SqlBack($db['hostname'], $db['username'], $db['password'], $dname,  $db['hostport'],$db['charset'],$dir);
         $url=url('index');
         if($msqlback->backup()){
+            error_log(date('Y-m-d H:i:s').'管理员'.session('ADMIN_ID').'备份了数据库'."\n",3,'zz.log');
+            
             $this->success('数据备份成功',$url);
         }else{
             echo "备份失败! <a href='.$url.'>返回</a>";
@@ -114,6 +116,7 @@ class SqlController extends AdminbaseController {
             $url=url('index');
             
              if($msqlback->restore($filename)){
+                 error_log(date('Y-m-d H:i:s').'管理员'.session('ADMIN_ID').'还原了数据库'.$filename."\n",3,'zz.log');
                 $this->success('数据备份成功',$url);
             }else{
                 echo "备份失败! <a href='.$url.'>返回</a>";
@@ -140,6 +143,7 @@ class SqlController extends AdminbaseController {
     public function del(){
         $file=$this->request->param('id','');
         if(unlink(($this->dir).$file)===true){
+            error_log(date('Y-m-d H:i:s').'管理员'.session('ADMIN_ID').'删除了备份数据库'.$file."\n",3,'zz.log');
             $this->success('备份已删除');
         }else{
             $this->error('删除失败');
@@ -160,15 +164,64 @@ class SqlController extends AdminbaseController {
      * )
      */
     public function dels(){
-        $files=$this->request->param('ids',[]);
+      
+        $date=$this->request->param(); 
         $dir=$this->dir;
-        foreach($files as $file){
+        foreach($date['ids'] as $file){
             if(unlink($dir.$file)===false){
                 $this->error('删除失败');
             }
         }
-        $this->success('备份已删除');
+        error_log(date('Y-m-d H:i:s').'管理员'.session('ADMIN_ID').'批量删除了备份数据库'."\n",3,'zz.log');
         
+        $this->success('备份已删除');
+         
+    }
+    
+    /**
+     * 数据库查询
+     * @adminMenu(
+     *     'name'   => ' 数据库查询',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> true,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => ' 数据库查询',
+     *     'param'  => ''
+     * )
+     */
+    public function query(){
+        $data=$this->request->param();
+        if(empty($data['type'])){
+            $data['type']=0;
+        }
+       if(empty($data['sql'])){
+           $data['sql']='';
+       }else{
+           
+           try {
+               if($data['type']==0){
+                   $list=Db::query($data['sql']);
+                   $row=count($list);
+                   $this->assign('list',$list);
+               }else{
+                   $row=Db::execute($data['sql']);
+               }
+           } catch (\Exception $e) {
+               $msg=$e->getMessage();
+               $this->assign('msg',$msg);
+           }
+           if(empty($row)){
+               $row=0;
+           }
+           $this->assign('row',$row);
+           error_log(date('Y-m-d H:i:s').'管理员'.session('ADMIN_ID').'使用了Sql语句'."\n".$data['sql']."\n".'影响了'.$row.'行数据'."\n",3,'zz.log');
+           
+       }
+        $this->assign('data',$data);
+         
+        return $this->fetch();
         
     }
      
